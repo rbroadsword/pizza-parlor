60b6d0d057cb45febf30b0654e49c47e3478d88b
Ryan Broadsword <rbroadsword@gmail.com>
2022-05-05 19:50:07 -0700

    WIP troubleshoot displaying price

--- js/scripts.js
+++ js/scripts.js
@@ -28,11 +28,27 @@ $(document).ready(function() {
     event.preventDefault(); 
   const firstName = $("#firstName").val(); 
   const lastName = $("#lastName").val(); 
-  const fullName = firstName + " " + lastName; 
+  const fullName = firstName + " " + lastName;
+  const address = $("#address").val(); 
+  const phoneNumber = $("#phone").val(); 
+  const size = $("#size").val();  
+  const crust = $("#crust").val(); 
   const toppings = []; 
   $("input:checkbox[name='topping']:checked").each(function() {
     toppings.push($(this).val());
   });
-  console.log(toppings);
+
+  let myOrder = new Pizza(size, crust, toppings);
+
+  myOrder.price(); 
+  let cost = myOrder.total; 
+  console.log(cost); 
+
+  $(".fullName").text(fullName); 
+  $(".display-size").text(size); 
+  $(".display-crust").text(crust); 
+  $(".display-toppings").text(toppings); 
+  $(".display-total").text(cost); 
+  console.log(myOrder);
   });
 })
