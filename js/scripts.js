d74f82c64fddf28d2c327ee1d3108ad0a0a94cf8
Ryan Broadsword <rbroadsword@gmail.com>
2022-05-05 18:42:01 -0700

    passed test one for Pizza.prototype.price function

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,23 @@
-//Business Logic 
+//Business Logic for Pizza ---
 
 function Pizza(size, crust, toppings) {
   this.size = size; 
   this.crust = crust; 
   this.toppings = toppings; 
+  this.total = 0; 
+}
+
+Pizza.prototype.price = function() { 
+  let numberOfToppings = 0; 
+
+  if (this.size === "large") {
+    this.total += 20; 
+  } else if (this.size === "medium") {
+    this.total += 15; 
+  }else if (this.size === "small") {
+    this.total += 10; 
+  } 
+   
+  numberOfToppings +=  parseInt(this.toppings.length); 
+  this.total += numberOfToppings * 1; 
 }
